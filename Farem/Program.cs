ac59fe15f4a40beff8884150eacd6050c5a5e23c
naunblisset <menmarcosdez@gmail.com>
2023-08-28 16:59:15 -0600

    Revert "Ahora sí"
    This reverts commit 4fe6eb6397e42036e6705328215a7625551e005d.

--- Farem/Program.cs
+++ Farem/Program.cs
@@ -1,4 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("la bibi ");
-
+Console.WriteLine("la bibi ");
+Console.WriteLine("la bibi ");
+Console.WriteLine("la bibi ");
+Console.WriteLine("la bibi ");
+Console.WriteLine("la bibi ");
+Console.WriteLine("la bibi ");
+Console.WriteLine("la bibi ");
+Console.WriteLine("la bibi ");
+Console.WriteLine("la bibi ");
+Console.WriteLine("la bibi ");
+Console.WriteLine("la bibi ");
 
