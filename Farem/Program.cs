4fe6eb6397e42036e6705328215a7625551e005d
Marielisef <143453161+Marielisef@users.noreply.github.com>
2023-08-28 16:42:05 -0600

    Ahora sí

--- Farem/Program.cs
+++ Farem/Program.cs
@@ -1,14 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("la bibi ");
-Console.WriteLine("la bibi ");
-Console.WriteLine("la bibi ");
-Console.WriteLine("la bibi ");
-Console.WriteLine("la bibi ");
-Console.WriteLine("la bibi ");
-Console.WriteLine("la bibi ");
-Console.WriteLine("la bibi ");
-Console.WriteLine("la bibi ");
-Console.WriteLine("la bibi ");
-Console.WriteLine("la bibi ");
-Console.WriteLine("la bibi ");
+
 
